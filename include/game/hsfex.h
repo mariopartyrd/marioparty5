d3ddd4ff5dd6ba9676e9dafe89c7974e021b6929
gamemasterplc <gamemasterplc@gmail.com>
2024-11-29 10:47:24 -0600

    Decompile hsfex

--- include/game/hsfex.h
+++ include/game/hsfex.h
@@ -2,14 +2,27 @@
 #define _GAME_HSFEX_H
 
 #include "dolphin.h"
+#include "humath.h"
+#include "game/hu3d.h"
 
-void CamMotionEx2(s16 arg0, s16 arg1, float arg2, s16 arg3);
-void CamMotionEx(s16 arg0, s16 arg1, Vec *arg2, Vec *arg3, Vec *arg4, float arg5, s16 arg6);
-float InterpolateBMLine(float *arg0, float *arg1, float arg2);
-void Hu3D2Dto3D(Vec *arg0, s16 arg1, Vec *arg2);
-void Hu3D3Dto2D(Vec *arg0, s16 arg1, Vec *arg2);
-void Hu3DMtxTransGet(Mtx arg0, Vec *arg1);
-void Hu3DMtxRotGet(Mtx arg0, Vec *arg1);
-void Hu3DMtxScaleGet(Mtx arg0, Vec *arg1);
+typedef struct CamMotionWork_s {
+    float time;
+    float dist;
+    float fov;
+    HuVecF pos;
+    HuVecF up;
+    HuVecF target;
+} CAMMOTIONWORK;
+
+void CamMotionEx2(HU3DMODELID modelId, s16 cameraBit, float maxTime, s16 mode);
+void CamMotionEx(HU3DMODELID modelId, s16 cameraBit, HuVecF *pos, HuVecF *up, HuVecF *target, float maxTime, s16 mode);
+int CamMotionExPathGet(HU3DMODELID modelId, s16 stepSpeed, float maxTime, s16 mode, CAMMOTIONWORK **workBuf, int *stepMax);
+void CamMotionExPath(s16 cameraBit, CAMMOTIONWORK *workBuf, int num);
+float InterpolateBMLine(float *p1, float *p2, float time);
+void Hu3D2Dto3D(HuVecF *src, s16 cameraBit, HuVecF *dst);
+void Hu3D3Dto2D(HuVecF *src, s16 cameraBit, HuVecF *dst);
+void Hu3DMtxTransGet(Mtx src, HuVecF *dst);
+void Hu3DMtxRotGet(Mtx src, HuVecF *dst);
+void Hu3DMtxScaleGet(Mtx src, HuVecF *dst);
 
 #endif
