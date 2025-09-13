4481740181ead2e3a6f0cf609eb62212176ced03
gamemasterplc <gamemasterplc@gmail.com>
2025-09-12 22:30:23 -0500

    Start capmain.c

---
+++ include/datanum/capsulechar1.h
@@ -0,0 +1,46 @@
+#ifndef DATANUM_CAPSULECHAR1_H
+#define DATANUM_CAPSULECHAR1_H
+
+#include "datadir_enum.h"
+
+enum {
+    CAPSULECHAR1_HSF_killer = DATANUM(DATA_capsulechar1, 0),
+    CAPSULECHAR1_HSF_hammer_bros = DATANUM(DATA_capsulechar1, 1),
+    CAPSULECHAR1_HSF_hammer_bros_idle = DATANUM(DATA_capsulechar1, 2),
+    CAPSULECHAR1_HSF_hammer_bros_throw = DATANUM(DATA_capsulechar1, 3),
+    CAPSULECHAR1_HSF_hammer_bros_end = DATANUM(DATA_capsulechar1, 4),
+    CAPSULECHAR1_HSF_hammer = DATANUM(DATA_capsulechar1, 5),
+    CAPSULECHAR1_HSF_kuru_hammer = DATANUM(DATA_capsulechar1, 6),
+    CAPSULECHAR1_HSF_hanachan = DATANUM(DATA_capsulechar1, 7),
+    CAPSULECHAR1_HSF_hanachan_idle = DATANUM(DATA_capsulechar1, 8),
+    CAPSULECHAR1_HSF_hanachan_lift = DATANUM(DATA_capsulechar1, 9),
+    CAPSULECHAR1_HSF_hanachan_jump = DATANUM(DATA_capsulechar1, 10),
+    CAPSULECHAR1_HSF_hanachan_fly = DATANUM(DATA_capsulechar1, 11),
+    CAPSULECHAR1_HSF_hanachan_flutter = DATANUM(DATA_capsulechar1, 12),
+    CAPSULECHAR1_HSF_hanachan_flutter_move = DATANUM(DATA_capsulechar1, 13),
+    CAPSULECHAR1_HSF_hanachan_flutter_hold = DATANUM(DATA_capsulechar1, 14),
+    CAPSULECHAR1_HSF_kamekku = DATANUM(DATA_capsulechar1, 15),
+    CAPSULECHAR1_HSF_kamekku_wait = DATANUM(DATA_capsulechar1, 16),
+    CAPSULECHAR1_HSF_kamekku_up = DATANUM(DATA_capsulechar1, 17),
+    CAPSULECHAR1_HSF_kamekku_down = DATANUM(DATA_capsulechar1, 18),
+    CAPSULECHAR1_HSF_kamekku_idle = DATANUM(DATA_capsulechar1, 19),
+    CAPSULECHAR1_HSF_magic_circle = DATANUM(DATA_capsulechar1, 20),
+    CAPSULECHAR1_HSF_magic_tri = DATANUM(DATA_capsulechar1, 21),
+    CAPSULECHAR1_HSF_magic_square = DATANUM(DATA_capsulechar1, 22),
+    CAPSULECHAR1_HSF_magic = DATANUM(DATA_capsulechar1, 23),
+    CAPSULECHAR1_HSF_kokamekku = DATANUM(DATA_capsulechar1, 24),
+    CAPSULECHAR1_HSF_kokamekku_wait = DATANUM(DATA_capsulechar1, 25),
+    CAPSULECHAR1_HSF_kokamekku_up = DATANUM(DATA_capsulechar1, 26),
+    CAPSULECHAR1_HSF_kokamekku_down = DATANUM(DATA_capsulechar1, 27),
+    CAPSULECHAR1_HSF_kokamekku_idle = DATANUM(DATA_capsulechar1, 28),
+    CAPSULECHAR1_HSF_bubble = DATANUM(DATA_capsulechar1, 29),
+    CAPSULECHAR1_HSF_togezo = DATANUM(DATA_capsulechar1, 30),
+    CAPSULECHAR1_HSF_throwman = DATANUM(DATA_capsulechar1, 31),
+    CAPSULECHAR1_HSF_throwman_mot = DATANUM(DATA_capsulechar1, 32),
+    CAPSULECHAR1_HSF_tsumuji = DATANUM(DATA_capsulechar1, 33),
+    CAPSULECHAR1_HSF_tsumuji_idle = DATANUM(DATA_capsulechar1, 34),
+    CAPSULECHAR1_HSF_tsumuji_in = DATANUM(DATA_capsulechar1, 35),
+    CAPSULECHAR1_HSF_tsumuji_kaze = DATANUM(DATA_capsulechar1, 36),
+};
+
+#endif
---
+++ include/datanum/capsulechar3.h
@@ -0,0 +1,28 @@
+#ifndef DATANUM_CAPSULECHAR3_H
+#define DATANUM_CAPSULECHAR3_H
+
+#include "datadir_enum.h"
+
+enum {
+    CAPSULECHAR3_HSF_dossun = DATANUM(DATA_capsulechar3, 0),
+    CAPSULECHAR3_HSF_dossun_idle = DATANUM(DATA_capsulechar3, 1),
+    CAPSULECHAR3_HSF_battan = DATANUM(DATA_capsulechar3, 2),
+    CAPSULECHAR3_HSF_battan_idle = DATANUM(DATA_capsulechar3, 3),
+    CAPSULECHAR3_HSF_battan_walk = DATANUM(DATA_capsulechar3, 4),
+    CAPSULECHAR3_HSF_donkey = DATANUM(DATA_capsulechar3, 5),
+    CAPSULECHAR3_HSF_donkey_idle = DATANUM(DATA_capsulechar3, 6),
+    CAPSULECHAR3_HSF_donkey_jump_start = DATANUM(DATA_capsulechar3, 7),
+    CAPSULECHAR3_HSF_donkey_jump = DATANUM(DATA_capsulechar3, 8),
+    CAPSULECHAR3_HSF_donkey_punch = DATANUM(DATA_capsulechar3, 9),
+    CAPSULECHAR3_HSF_donkey_walk = DATANUM(DATA_capsulechar3, 10),
+    CAPSULECHAR3_HSF_donkey_roll = DATANUM(DATA_capsulechar3, 11),
+    CAPSULECHAR3_HSF_donkey_sai_hit = DATANUM(DATA_capsulechar3, 12),
+    CAPSULECHAR3_HSF_donkey_sai_fall = DATANUM(DATA_capsulechar3, 13),
+    CAPSULECHAR3_HSF_donkey_win = DATANUM(DATA_capsulechar3, 14),
+    CAPSULECHAR3_HSF_donkey_lose = DATANUM(DATA_capsulechar3, 15),
+    CAPSULECHAR3_HSF_donkey_punch_hard = DATANUM(DATA_capsulechar3, 16),
+    CAPSULECHAR3_HSF_donkey_sad = DATANUM(DATA_capsulechar3, 17),
+    CAPSULECHAR3_HSF_donkey_anger = DATANUM(DATA_capsulechar3, 18),
+};
+
+#endif
